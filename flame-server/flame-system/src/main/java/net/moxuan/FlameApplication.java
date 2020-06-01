f6f4be94f714685045b03309cd35b2206403ce61
moxuan <350521180@qq.com>
2020-06-01 21:59:46 +0800

    新建 springboot服务端

---
+++ flame-server/flame-system/src/main/java/net/moxuan/FlameApplication.java
@@ -0,0 +1,16 @@
+package net.moxuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: moxuan
+ * @date: 2020-6-1
+ * @description: Springboot 启动类
+ */
+@SpringBootApplication
+public class FlameApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FlameApplication.class, args);
+    }
+}
